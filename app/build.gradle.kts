6ce5aad7fca7c0bdf677ffe8ef9203e50f552878
Xiaojian Wang <xiaojianw@saharalabs.ai>
2024-10-26 06:31:50 +0000

    Upgrade spotless to 7.0.0.BETA4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
-    id("com.diffplug.spotless") version "7.0.0.BETA2"
+    id("com.diffplug.spotless") version "7.0.0.BETA4"
 }
 
 repositories {
